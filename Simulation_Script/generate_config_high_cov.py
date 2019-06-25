bbcc5139aee073a823c3fffbfa7a6255d21bfaa4
David Helekal <david.helekal@gmail.com>
2019-06-25 15:31:56 +0100

    Updated  config gens

--- Simulation_Script/generate_config_high_cov.py
+++ Simulation_Script/generate_config_high_cov.py
@@ -7,34 +7,34 @@ random.seed(1234567890)
 for i in range(0,5):
     
     data = {'config':{
-        'name':'Experiment_HiDense_'+str(i),
+        'name':'Experiment_Med_Cov_'+str(i),
         'random_seed':random.randint(1, 1234567890),
-        'n_patterns':8,
+        'n_patterns':4,
         'n_histones':10,
         'n_modifications':10,
         'locations_per_pattern':300,
-        'sampling_depth_foreground':60,
-        'sampling_depth_background':20
+        'sampling_depth_foreground':330,
+        'sampling_depth_background':120
     },
     'cluster':{
         'prob':0.4
-        'intensity_alpha':5.0,
-        'intensity_beta':1.0
+        'intensity_alpha':0.8,
+        'intensity_beta':0.4
     },
     'histone':{
         'width':300,
-        'sigma':150
+        'sigma':100
     },
     'foreground_prior_beta':{
-        'alpha': 0.5,
-        'beta': 0.5
+        'alpha': 0.3,
+        'beta': 0.2
     },
     'background_prior_beta':{
         'alpha': 1.0,
         'beta': 1.0
     }}
     jstr = json.dumps(data, indent=4)
-    with open("configHiDense"+str(i)+".json", "w") as text_file:
+    with open(data['config']['name']+"conf"+".json", "w") as text_file:
         print(jstr, file=text_file)
 
 
