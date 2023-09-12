99d6e748d96157700ce344f8c851c7c01eaf3f9a
Tanmayee Narendra <n.tanmayee@gmail.com>
2023-09-12 14:59:04 +0100

    updated new simulation

--- epi_cluster/simulate.py
+++ epi_cluster/simulate.py
@@ -13,7 +13,18 @@ class Params(object):
     pass
 
 
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 def draw_sample(tensor, index, mask):
+    logging.debug('Tensor shape: %s' % str(tensor.shape))
+    logging.debug('Index shape: %s' % str(index.shape))
+    logging.debug('Mask shape: %s' % str(mask.shape))
+
     tensor_f = tf.reshape(tensor, [-1])
     index_f = tf.reshape(index, [-1])
     mask_f = tf.reshape(mask, [-1])
@@ -46,9 +57,9 @@ class Simulation(object):
         self.background_prior_beta = params.background_prior_beta
         self.background_prior_alpha = params.background_prior_alpha
         
-        self.patterns = self.generate_patterns()
-        self.session = tf.InteractiveSession()
+        self.generate_patterns()
         self.save_config()
+        self.writer = tf.summary.create_file_writer(join(self.output_dir, 'tf_log'))
 
     def run_simulation(self):
         logging.info('Started run_simulation()')
@@ -56,37 +67,38 @@ class Simulation(object):
         for i in range(self.n_patterns):
             for j in range(len(self.patterns[i])):
                 confounding_vector = tf.constant(self.patterns[i][j], dtype=tf.float32)
-                confounding_vector *= tfp.distributions.Beta(self.cluster_intensity_alpha, self.cluster_intensity_beta).sample()
-
+                confounding_vector *= tfp.distributions.Beta(self.cluster_intensity_alpha, self.cluster_intensity_beta).sample([1])
                 cluster, _, confounder_distribution = self.create_cluster(confounding_vector)
-                with tf.name_scope('summaries'):
-                    tf.summary.tensor_summary("Association "+str(i)+" Cluster "+str(j), confounder_distribution.probs)
+        
                 if j > 0:
                     clusters = tf.concat([cluster, clusters],axis=1)
                 else:
                     clusters = cluster
+
+                with self.writer.as_default():
+                    tf.summary.write(f'Association {i} Cluster {j}', confounder_distribution.probs, step=1)
             
             # sample background reads
             draws_background = self.create_nucleosome_draws(type='background')
             driver_background, _ = self.create_random_drivers()
             draws_background = self.flatten_reshape(draws_background)
             driver_background = self.flatten_reshape(driver_background)
-            index_background = self.create_mod_index()
+            index_background = self.create_mod_index(type='background')
             background_samples = draw_sample(draws_background, index_background, driver_background)
 
             # run and write to file
             for j in range(self.locations_per_pattern):
                 logging.info('Simulating reads...')
-                _ = tf.summary.merge_all()
-                out = self.session.run(
-                    {'background': tf.transpose(background_samples, [1,0]), 
-                     'foreground': tf.transpose(clusters, [1,0])})
+                # _ = tf.summary.merge_all()
+                out = {'background': tf.transpose(background_samples, [1,0]), 'foreground': tf.transpose(clusters, [1,0])}
             
                 background_df = pd.DataFrame({'position': out['background'][:,0], 'modification': out['background'][:,1]})
                 foreground_df = pd.DataFrame({'position': out['foreground'][:,0], 'modification': out['foreground'][:,1]})
 
                 df = pd.concat([background_df, foreground_df])
-                self.write_reads_as_bed(df, f'peak_{j}')
+                self.write_reads_as_bed(df, f'pattern_{i}_peak_{j}')
+        
+        self.writer.flush()
 
     def write_reads_as_bed(self, df, name):
         logging.info('Writing reads to %s' % name)
@@ -101,17 +113,19 @@ class Simulation(object):
 
     def generate_patterns(self):
         logging.info('Generating enrichment patterns...')
-        perm = np.random.permutation(self.n_modifications)
-        size_sum = 0
-        clusters = []
-        while size_sum < self.n_modifications:
-            clust_sz = np.random.binomial(self.n_modifications-size_sum-1, self.cluster_prob, 1) + 1
-            clust_vec = np.pad(np.ones(clust_sz), (size_sum, self.n_modifications-size_sum-clust_sz), 'constant', constant_values=(0,0))
-            clust_vec = clust_vec[perm]
-            
-            clusters.append(clust_vec)
-            size_sum = size_sum+clust_sz[0]
-        return(clusters)
+        self.patterns = []
+        for _ in range(self.n_patterns):
+            perm = np.random.permutation(self.n_modifications)
+            size_sum = 0
+            clusters = []
+            while size_sum < self.n_modifications:
+                clust_size = int(np.random.binomial(self.n_modifications-size_sum-1, self.cluster_prob, 1) + 1)
+                clust_vec = np.pad(np.ones(clust_size), (size_sum, self.n_modifications-size_sum-clust_size), 'constant', constant_values=(0,0))
+                clust_vec = clust_vec[perm]
+                
+                clusters.append(clust_vec)
+                size_sum = size_sum+clust_size
+            self.patterns.append(clusters)
     
     def save_config(self, indent=3):
         logging.info('Saving config/patterns to file...')
@@ -119,11 +133,14 @@ class Simulation(object):
 
         patterns_to_save = [list(p) for p in self.patterns]
         with open(join(self.output_dir, 'patterns.json'), 'w') as fp:
-            json.dump(patterns_to_save, fp, indent=indent)
+            json.dump(patterns_to_save, fp, indent=indent, cls=NumpyEncoder)
+
+        logging.info('Saving config parameters to file...')
+        logging.info(json.dumps(vars(self), indent=1, cls=NumpyEncoder))
         with open(join(self.output_dir, 'config.txt'), 'w') as fp:
             import datetime
             fp.write(f'Time of start: {datetime.datetime.now()}\n\n')
-            fp.write(json.dumps(vars(self), indent=indent))
+            fp.write(json.dumps(vars(self), indent=indent, cls=NumpyEncoder))
 
     def create_cluster(self, confounding_vector):
         logging.info('Creating a cluster of modifications...')
@@ -133,16 +150,23 @@ class Simulation(object):
         foreground_draws = self.flatten_reshape(foreground_draws)
         driver = self.flatten_reshape(driver)
 
-        index = self.create_mod_index()
+        index = self.create_mod_index(type='foreground')
         samples = draw_sample(foreground_draws, index, driver)
         
         return(samples, nucleosome_distribution, confounder_distribution)
     
-    def create_mod_index(self):
+    def create_mod_index(self, type):
+        if type == 'foreground':
+            n_reads = self.sampling_depth_foreground
+        elif type == 'background':
+            n_reads = self.sampling_depth_background
+        else:
+            raise ValueError(f'Unknown type `{type}`: choose foreground/background')
+
         logging.info('In create_mod_index()...')
         return tf.broadcast_to(
             tf.range(self.n_modifications), 
-            [self.sampling_depth_foreground * self.n_histones, self.n_modifications])  ####[N*self.n_histones, self.n_modifications]
+            [n_reads * self.n_histones, self.n_modifications])  ####[N*self.n_histones, self.n_modifications]
     
     def create_confounding_drivers(self, confounding_vector):
         logging.info('Creating confounding drivers...')
@@ -184,7 +208,7 @@ class Simulation(object):
                     self.histone_width / 2, (self.n_histones - 1) * self.histone_width + self.histone_width / 2, self.n_histones), 
                 [self.n_modifications]), 
             [self.n_modifications, self.n_histones])
-        truncated_normal_draws = tf.truncated_normal([N, self.n_modifications, self.n_histones], mean=0.0, stddev=self.histone_sigma, dtype=tf.float32)
+        truncated_normal_draws = tf.random.truncated_normal([N, self.n_modifications, self.n_histones], mean=0.0, stddev=self.histone_sigma, dtype=tf.float32)
         location_draws = tf.add(truncated_normal_draws, means)
         location_draws = location_draws - offset
         return (location_draws) ###[N x self.n_modifications x self.n_histones]
@@ -202,7 +226,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.basicConfig(format='%(asctime)s %(levelname)s\t%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     parser = argparse.ArgumentParser(prog = 'epi_cluster', description = 'Simulate Chip-Seq for correlated histone modifications')
     parser.add_argument('params')
     args = parser.parse_args()
